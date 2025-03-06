42539170690a7186473454e579b464bf29b50913
Mitshukaa <118875043+Mitshukaa@users.noreply.github.com>
2025-03-06 10:27:42 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MitshuStore/'
+  base: '/MitshuStore/',  // Mengonfigurasi base path untuk GitHub Pages
+  build: {
+    outDir: 'dist',  // Pastikan hasil build ditempatkan di folder 'dist'
+  }
 })
-
